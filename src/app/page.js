b6603b10e103d447dad853a30e12ca032fb0ca29
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-03-16 15:21:43 +0530

    feat: Fetch and render product details in productCard

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
+import axios from "axios";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setProducts(response.data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="m-10 flex flex-col gap-8">
       <h1 className="text-center text-5xl font-medium bg-gradient-to-r from-red-200 via-orange-400 to-yellow-700 text-transparent bg-clip-text">
@@ -53,8 +66,10 @@ export default function Home() {
       </div>
 
       {/* Product Card */}
-      <div className="mt-8">
-        <ProductCard />
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </div>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,33 @@
 import Image from "next/image";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
-    <div className="w-80 bg-white rounded-2xl shadow-lg border border-gray-300 overflow-hidden">
+    <div className="sm:max-w-sm bg-white rounded-2xl shadow-lg border border-gray-300 overflow-hidden flex flex-col h-[450px]">
       {/* Top Section */}
-      <div className="relative bg-gradient-to-b from-purple-300 to-purple-500 h-72 flex justify-center items-center">
+      <div className="relative h-72 flex justify-center items-center border-b border-gray-300">
         <Image
-          src="/shoe.jpg"
+          src={product.image}
           alt="Nike Running Shoe"
           layout="fill"
-          objectFit="cover"
+          // objectFit="cover"
         />
       </div>
 
       {/* Bottom Section */}
       <div className="p-5">
-        <h2 className="text-lg font-bold text-orange-500">Nike Running Shoe</h2>
+        <h2 className="text-lg font-bold text-orange-500 truncate">{product.title}</h2>
 
         {/* Description */}
         <p className="text-sm text-gray-600 mt-3">
-          {`Crossing hardwood comfort with off-court flair. '80s-inspired construction, bold details, and nothing but net style.`
+          {product.description
             .split(" ")
-            .slice(0, 30)
+            .slice(0, 15)
             .join(" ")}
         </p>
 
         {/* Price & Button */}
         <div className="mt-4 flex justify-between items-center">
-          <p className="text-xl font-bold text-gray-900">$69.99</p>
+          <p className="text-xl font-bold text-gray-900">${product.price}</p>
           <button className="bg-orange-600 text-white px-4 py-2 rounded-lg font-semibold shadow-md hover:bg-orange-500 cursor-pointer">
             Add to cart
           </button>
